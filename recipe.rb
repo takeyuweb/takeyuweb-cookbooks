5fa0eff08f5090bdf2f7af9ce9b44cc9b10866dc
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2018-01-31 13:04:18 +0900

    Add Node.JS

--- recipe.rb
+++ recipe.rb
@@ -1,15 +1,17 @@
 include_recipe 'rbenv::user'
 
+execute 'apt-get update' do
+  subscribes :run, 'execute[add-ubuntu-repository]', :immediately
+  subscribes :run, 'execute[add-yarn-repository]', :immediately
+  action :nothing
+end
+
 # Docker CE
 package 'apt-transport-https'
 package 'ca-certificates'
 package 'curl'
 package 'software-properties-common'
 execute 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -'
-execute 'apt-get update' do
-  subscribes :run, 'execute[add-ubuntu-repository]', :immediately
-  action :nothing
-end
 execute 'add-ubuntu-repository' do
   command 'add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
 end
@@ -18,3 +20,13 @@ package 'docker-compose'
 execute 'usermod -aG docker takeyuweb' do
   not_if 'groups takeyuweb | grep docker -w'
 end
+
+# Node.js
+execute 'curl -sL https://deb.nodesource.com/setup_8.x | bash -'
+package 'nodejs'
+# Yarn
+execute 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -'
+execute 'add-yarn-repository' do
+  command 'echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list'
+end
+package 'yarn'
