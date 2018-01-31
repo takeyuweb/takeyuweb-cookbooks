62c215ebe7b7520671dd88fb78613a517ff65b3d
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2018-01-31 14:05:51 +0900

    Change a docker-compose source
    apt get で入るバージョンは古いので

--- recipe.rb
+++ recipe.rb
@@ -38,9 +38,16 @@ execute 'add-ubuntu-repository' do
   command 'add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
 end
 package 'docker-ce'
-package 'docker-compose'
 execute 'usermod -aG docker takeyuweb' do
   not_if 'groups takeyuweb | grep docker -w'
+end
+execute 'chmod +x /usr/local/bin/docker-compose' do
+  subscribes :run, 'execute[download-docker-compose]'
+  action :nothing
+end
+execute 'download-docker-compose' do
+  command 'curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose'
+  not_if 'docker-compose -v | grep "1.18.0"'
 end
 
 # Node.js
